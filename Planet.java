1e7cc627c3a1f43d1ec3b4a45f9a2e5faa928c12
cirodelia74 <85739966+cirodelia74@users.noreply.github.com>
2021-06-16 10:48:31 +0200

    Update Planet.java

--- Planet.java
+++ Planet.java
@@ -2,57 +2,54 @@ import java.util.Arrays;
 import java.lang.Math;
 
 public class Planet {
-	
-	// incapsulamento degli attributi della classe
+    
+    // incapsulamento degli attributi della classe
     private Integer[] board;
     private int turn;
 
     public Planet(String input){
-    	
-    	board = new Integer[input.length()];
-    	char c;
-    	//inizializzo board con l'input ricevuto
-    	for (int i=0; i<input.length(); i++) {
-    		c = input.charAt(i);
-    		board[i] = Character.getNumericValue(c);
-    	}
-    		//board[i] = Integer.parseInt(input.substring(i, 1));
-    	
-    	// inizializzo il turno del gioco
-    	turn = 0;
-    	
+        
+        board = new Integer[input.length()];
+        char c;
+        //inizializzo board con l'input ricevuto
+        for (int i=0; i<input.length(); i++) {
+            c = input.charAt(i);
+            board[i] = Character.getNumericValue(c);
+        }
+            //board[i] = Integer.parseInt(input.substring(i, 1));
+        
+        // inizializzo il turno del gioco
+        turn = 0;
+        
     }
 
     public void printBoard(){
 
-    	// stampo la situazione attuale di board
-    	for (int i=0; i<board.length; i++)
-    		System.out.print(board[i] +" ");
-    	System.out.println();
+        // stampo la situazione attuale di board
+        for (int i=0; i<board.length; i++)
+            System.out.print(board[i] +" ");
+        System.out.println();
     }
 
     // restituisce true se c'è almeno un 1 nel board, altrimenti false
     public boolean lifeOnPlanet(){
         
-        boolean life = false;
-        int count = 0;
         // se almeno un elemento di board e' 1 esco dal metodo
-        while (count < board.length && !life) {
-            if (board[count] == 1)
+        for (int i=0; i<board.length; i++) {
+            if (board[i] == 1)
                 return true;
-            count++;
         }
-        return life;
+        return false;
     }
     
     // basandosi sul board attuale, lo aggiorna per il successivo turno sequendo le seguenti regole:
     // PER ogni elemento i del board SE:
-    //  vicini sono entrambi 1 ed i è 0 -> i diventa 1 (nuova nascita)		1, 0, 1 -> 1, 1, 1
-    //  vicini sono entrambi 1 ed i è 1 -> i diventa 0 (morte per sovrappopolazione)	1, 1, 1 -> 1, 0, 1
-    //  vicini sono uno 0 e l'altro 1	 -> i non cambia (sopravvivenza)		1, 1, 0 -> 1, 1, 0
-    //											0, 1, 1 -> 0, 1, 1
-    //  vicini sono entrambi 0     -> i diventa 0 nel (morte per sottopopolazione)	0, 1, 0 -> 0, 0, 0
-    //											0, 0, 0 -> 0, 0, 0
+    //  vicini sono entrambi 1 ed i è 0 -> i diventa 1 (nuova nascita)      1, 0, 1 -> 1, 1, 1
+    //  vicini sono entrambi 1 ed i è 1 -> i diventa 0 (morte per sovrappopolazione)    1, 1, 1 -> 1, 0, 1
+    //  vicini sono uno 0 e l'altro 1    -> i non cambia (sopravvivenza)        1, 1, 0 -> 1, 1, 0
+    //                                          0, 1, 1 -> 0, 1, 1
+    //  vicini sono entrambi 0     -> i diventa 0 nel (morte per sottopopolazione)  0, 1, 0 -> 0, 0, 0
+    //                                          0, 0, 0 -> 0, 0, 0
     // Il board va considerato CIRCOLARE (es: il vicino sx di i=0 è i=board.length-1), e il metodo deve anche aggiornare il count del turno
     public void oneTurn(){
         // TODO: implement
